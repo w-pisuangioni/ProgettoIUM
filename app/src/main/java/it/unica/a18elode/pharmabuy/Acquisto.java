875ddfe001e6057d6231013e8c896b6abfe9c651
Marco1693 <giallo022@live.it>
2018-09-12 18:50:45 +0200

    popup Acquisto

--- app/src/main/java/it/unica/a18elode/pharmabuy/Acquisto.java
+++ app/src/main/java/it/unica/a18elode/pharmabuy/Acquisto.java
@@ -1,8 +1,11 @@
 package it.unica.a18elode.pharmabuy;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
@@ -33,6 +36,8 @@ public class Acquisto extends AppCompatActivity implements NavigationView.OnNavi
     private TextView prezzo;
     private TextView ricetta;
     private Button buy;
+    Dialog myDialog;
+
 
     Date date = new Date();
 
@@ -72,7 +77,8 @@ public class Acquisto extends AppCompatActivity implements NavigationView.OnNavi
         ricetta.setText(Medicinali.getClickedFarmaco().getRicetta());
 
 
-        init();
+        //init();
+        myDialog = new Dialog(this);
     }
     @Override
     public boolean onCreateOptionsMenu(android.view.Menu menu) {
@@ -80,20 +86,21 @@ public class Acquisto extends AppCompatActivity implements NavigationView.OnNavi
         return true;
     }
 
-    public void init() {
-        buy = (Button) findViewById(R.id.button_acquisto);
-        buy.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+    public void ConfAcquisto(View v) {
+       // buy = (Button) findViewById(R.id.btnfollow);
+       // buy.setOnClickListener(new View.OnClickListener() {
+          //  @Override
+           // public void onClick(View v) {
                 Intent goToMed = new Intent(Acquisto.this, Riepilogo.class);
                 Medicinali.getClickedFarmaco().setFarmaciaAcquisto(MedicinaleScelto.getClickedFarmacia());
                 Medicinali.getClickedFarmaco().setData(date);
                 AcquistiRecenti.addAcquisto(Medicinali.getClickedFarmaco());
                 //CustomAdapterAcquisti.addFarmaciaAcquisti(MedicinaleScelto.getClickedFarmacia());
+
                 startActivity(goToMed);
-            }
-        });
-        
+       //     }
+       // });
+
 
     }
 
@@ -144,5 +151,49 @@ public class Acquisto extends AppCompatActivity implements NavigationView.OnNavi
             startActivity(goToFarm);
         }
         return false;
+    }
+
+    public void ShowPopup(View v) {
+        TextView txtclose;
+        ImageView popMed;
+
+
+        Button btnFollow;
+        myDialog.setContentView(R.layout.custompopup);
+        txtclose =(TextView) myDialog.findViewById(R.id.txtclose);
+        txtclose.setText("X");
+
+        //nome med
+        TextView popName = myDialog.findViewById(R.id.pop_medicinale);
+        popName.setText(Medicinali.getClickedFarmaco().getNome());
+
+        //immagine popup
+        popMed=(ImageView)myDialog.findViewById(R.id.pop_img);
+        Context context = popMed.getContext();
+        int id = context.getResources().getIdentifier(Medicinali.getClickedFarmaco().getImage(), "drawable", context.getPackageName());
+        popMed.setImageResource(id);
+
+        //farmacia popup
+        TextView popFarmacia=(TextView)myDialog.findViewById(R.id.pop_farmacia);
+        popFarmacia.setText(MedicinaleScelto.getClickedFarmacia().getNome()+("\n")+(MedicinaleScelto.getClickedFarmacia().getCitta()));
+
+        //prezzo
+        Float prz=Medicinali.getClickedFarmaco().getPrezzo();
+        TextView popPrezzo=(TextView)myDialog.findViewById(R.id.pop_prezzo);
+        popPrezzo.setText(prz.toString()+(" €"));
+
+        //tipo
+        TextView popTipo =(TextView)myDialog.findViewById(R.id.pop_tipo);
+        popTipo.setText(Medicinali.getClickedFarmaco().getTipo());
+
+        btnFollow = (Button) myDialog.findViewById(R.id.btnfollow);
+        txtclose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myDialog.dismiss();
+            }
+        });
+        myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        myDialog.show();
     }
 }
